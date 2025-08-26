5dcb1eb8fd1da03268f6d22c19e06ec06f687ad2
Gabriel Espindola <zgabesp@xead.ericsson.com>
2025-08-26 15:19:34 -0300

    moving activations constructors to hpp

--- src/network/include/activations.hpp
+++ src/network/include/activations.hpp
@@ -2,19 +2,29 @@
 #define ACTIVATIONS_HPP
 
 #include "activation.hpp"
+#include "math_utils.hpp"
 
 template<typename T>
 class Tanh: public Activation<T>{
     public:
 
-    Tanh<T>();
+    //Constructor
+    Tanh<T>(): Activation<T>(
+        [](dlib::matrix<T> x) { return dlib::tanh(x); },
+        [](dlib::matrix<T> x) { return tanh_prime(x); }
+    ) {}
+
 };
 
 template<typename T>
 class Sigmoid: public Activation<T>{
     public:
 
-    Sigmoid<T>();
+    // Constructor
+    Sigmoid<T>(): Activation<T> (
+        [](dlib::matrix<T> x) { return dlib::sigmoid(x); }, 
+        [](dlib::matrix<T> x) { return sigmoid_prime(x); }
+    ) {}
 };
 
 #endif // ACTIVATIONS_HPP
